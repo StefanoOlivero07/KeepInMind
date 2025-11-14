668f29b5a2b17c3aea9283797a2cf48edb3131fc
Stefano Olivero <s.olivero.3215@vallauri.edu>
2025-11-14 14:22:04 +0100

    Created login service and db_backup folder
    Installed bcrpyt and linked it in package.json. Developed login service with password hash service

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -4,5 +4,9 @@ export class Configuration {
     port: number = 3000;
     connectionString: string = "mongodb://localhost:27017";
     dbName = "keep_in_mind";
+    dbCollections = {
+        "usersCollection": "users",
+        "tasksCollection": "tasks"
+    };
     postParametersSize: string = "5mb";
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,9 @@
 import http from "http";
 import fs from "fs";
 import express from "express";
-import { Condition, MongoClient } from "mongodb";
+import { Collection, Condition, MongoClient, MongoError } from "mongodb";
 import { Configuration } from "./configuration";
+import bcrypt from "bcrypt";
 
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
@@ -12,7 +13,7 @@ const config: Configuration = new Configuration();
 let errorPage: string = "";
 
 server.listen(config.port, () => {
-    console.log("Server is listening on port: " + config.port);
+    console.info("Server is listening on port: " + config.port);
 });
 
 // ---------- MIDDLEWARES ----------
@@ -36,6 +37,48 @@ app.use("/", (req, res, next) => {
 });
 
 //#endregion
+
+// ---------- DYNAMIC RESOURCES ----------
+app.post("/api/login", async (req, res, next) => {
+    if (req.body.email && req.body.password) {
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+        const client: MongoClient = new MongoClient(config.connectionString);
+
+        await client.connect().catch((err) => {
+            console.error("Database connection refused");
+            res.status(503).send("Database connection refused");
+            return;
+        });
+
+        const collection: Collection = client.db(config.dbName).collection(config.dbCollections.usersCollection);
+        const cmd = collection.findOne({email});
+
+        cmd
+            .catch((err: MongoError) => {
+                res.status(500).send("Query error: " + err.message);
+            })
+            .then(async (data) => {
+                if (!data) return res.status(401).send("Invalid credentials");
+
+                const passwordValid = await bcrypt.compare(password, data.password);
+                if (!passwordValid) return res.status(401).send("Invalid credentials");
+
+                res.send({
+                    _id: data._id,
+                    name: data.name,
+                    surname: data.surname
+                });
+            })
+            .finally(() => {
+                client.close();
+            });
+        return;
+    }
+
+    res.status(500).send("Missing parameters");
+    return;
+});
 
 // ---------- DEFAULT ROUTES ----------
 //#region 
