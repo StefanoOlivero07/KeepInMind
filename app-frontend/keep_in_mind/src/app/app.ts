acc17a8b54f82bee71ce24b648871ee662ccf38b
StefanoOlivero07 <s.olivero.3215@vallauri.edu>
2025-12-22 11:01:09 +0100

    Header, task and completed-task components
    TODO: rename 'completed-task' in 'completed-tasks'

--- app-frontend/keep_in_mind/src/app/app.ts
+++ app-frontend/keep_in_mind/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { CompletedTask } from "./completed-task/completed-task";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, CompletedTask],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
