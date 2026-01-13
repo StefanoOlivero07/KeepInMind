030f49c3dd234f916c852b22e22c913589635247
StefanoOlivero07 <s.olivero.3215@vallauri.edu>
2026-01-13 14:08:05 +0100

    Core export project
    Distinguished completed tasks file and not completed tasks file

---
+++ app-backend/exports/Exports_Solution/KeepInMindExports.Core/Models/Task.cs
@@ -0,0 +1,32 @@
+﻿namespace KeepInMindExports.Core.Models
+{
+    public class Task
+    {
+        private int _id;
+        private string? _title;
+        private string? _description;
+        private string? _category;
+        private string? _created;
+        private string? _expiration;
+        private string? _notes;
+        private bool _completed;
+        private string? _completedAt;
+        private string? _userId;
+
+        public int Id { get => _id; set => _id = value; }
+        public string? Title { get => _title; set => _title = value; }
+        public string? Description { get => _description; set => _description = value; }
+        public string? Category { get => _category; set => _category = value; }
+        public string? Created { get => _created; set => _created = value; }
+        public string? Expiration { get => _expiration; set => _expiration = value; }
+        public string? Notes { get => _notes; set => _notes = value; }
+        public bool Completed { get => _completed; set => _completed = value; }
+        public string? CompletedAt { get => _completedAt; set => _completedAt = value; }
+        public string? UserId { get => _userId; set => _userId = value; }
+
+        public override string ToString()
+        {
+            return $"{_id} - {_title} - {_description} - {_userId}";
+        }
+    }
+}
---
+++ app-backend/exports/Exports_Solution/KeepInMindExports.Core/OfficeAutomation/WordAutomation.cs
@@ -0,0 +1,62 @@
+﻿using KeepInMindExports.Core.Models;
+using System.Text.RegularExpressions;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace KeepInMindExports.Core.OfficeAutomation
+{
+    public static class WordAutomation
+    {
+        public static byte[] GetTasksDocxBytes(List<Models.Task> tasks, User user, string templatePath, bool completedTasks)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var document = DocX.Load(templatePath))
+            {
+                /*
+                ---------- User and document informations ----------
+                */
+                ReplaceSimpleTextInDocument(document, "{userName}", user.Name);
+                ReplaceSimpleTextInDocument(document, "{userSurname}", user.Surname);
+                ReplaceSimpleTextInDocument(document, "{userEmail}", user.Email);
+                ReplaceSimpleTextInDocument(document, "{documentCreation}", new DateTime().ToString("dd-mm-yyyy"));
+
+                /*
+                ---------- Tasks informations ----------
+                */
+                Table table = document.Tables[0];
+                Row newRow;
+
+                foreach (var task in tasks)
+                {
+                    newRow = table.InsertRow();
+                    newRow.Cells[0].Paragraphs[0].Append(task.Title);
+                    newRow.Cells[1].Paragraphs[0].Append(task.Description);
+                    newRow.Cells[2].Paragraphs[0].Append(task.Category);
+                    newRow.Cells[3].Paragraphs[0].Append(task.Created);
+                    if (completedTasks)
+                        newRow.Cells[4].Paragraphs[0].Append(task.CompletedAt);
+                    else
+                        newRow.Cells[4].Paragraphs[0].Append(task.Expiration);
+                    newRow.Cells[5].Paragraphs[0].Append(task.Notes);
+                }
+
+                document.SaveAs(memoryStream);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        private static void ReplaceSimpleTextInDocument(DocX document, string oldText, string? newText)
+        {
+            var options = new StringReplaceTextOptions
+            {
+                SearchValue = oldText,
+                NewValue = newText ?? string.Empty,
+                RegExOptions = RegexOptions.None,
+                NewFormatting = new Formatting() { Bold = true, FontColor = Xceed.Drawing.Color.Black }
+            };
+
+            document.ReplaceText(options);
+        }
+    }
+}
